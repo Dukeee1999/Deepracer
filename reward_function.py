87f45c68763b84d30ea86f201312d2bcea795fb2
Dukeee1999 <62459234+Dukeee1999@users.noreply.github.com>
2020-08-10 16:06:08 +1000

    Add files via upload

---
+++ reward_function.py
@@ -0,0 +1,40 @@
+def reward_function(params):
+
+    '''
+        In @params object:
+    {
+        "all_wheels_on_track": Boolean,    # flag to indicate if the vehicle is on the track
+        "x": float,                        # vehicle's x-coordinate in meters
+        "y": float,                        # vehicle's y-coordinate in meters
+        "distance_from_center": float,     # distance in meters from the track center 
+        "is_left_of_center": Boolean,      # Flag to indicate if the vehicle is on the left side to the track center or not. 
+        "heading": float,                  # vehicle's yaw in degrees
+        "progress": float,                 # percentage of track completed
+        "steps": int,                      # number steps completed
+        "speed": float,                    # vehicle's speed in meters per second (m/s)
+        "streering_angle": float,          # vehicle's steering angle in degrees
+        "track_width": float,              # width of the track
+        "waypoints": [[float, float], … ], # list of [x,y] as milestones along the track center
+        "closest_waypoints": [int, int]    # indices of the two nearest waypoints.
+    }
+    '''
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+    
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+    
+    return float(reward)
